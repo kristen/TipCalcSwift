408044ea2dcbbdfa3d623ad9e82a5e465242226c
Kristen Sundquist <kristen@sigfig.com>
2014-12-22 22:56:01 -0500

    Improved design and add split bill functionality

--- TipCalcSwift/ViewController.swift
+++ TipCalcSwift/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var halfTotalLabel: UILabel!
+    @IBOutlet weak var thirdTotalLabel: UILabel!
+    @IBOutlet weak var forthTotalLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,18 +28,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    
     @IBAction func onEditingChange(sender: AnyObject) {
+        
         let tipPercentages = [0.15, 0.18, 0.2]
         var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
         
-        var billAmount = (billField.text as NSString).doubleValue
+        var billString = billField.text.stringByTrimmingCharactersInSet(NSCharacterSet.symbolCharacterSet())
+        var billAmount = (billString as NSString).doubleValue
 
         var tip = billAmount * tipPercentage
         var total = billAmount + tip
+        var halfTotal = total / 2
+        var thirdTotal = total / 3
+        var forthTotal = total / 4
 
         tipLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
+        halfTotalLabel.text = String(format: "$%.2f", halfTotal)
+        thirdTotalLabel.text = String(format: "$%.2f", thirdTotal)
+        forthTotalLabel.text = String(format: "$%.2f", forthTotal)
     }
     
     @IBAction func onTap(sender: AnyObject) {
