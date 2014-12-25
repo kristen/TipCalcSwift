486c146238188d29318d7900bb8005eb1793ec51
Kristen Sundquist <kristen@sigfig.com>
2014-12-25 16:25:36 -0500

    Refactor code

--- TipCalcSwift/ViewController.swift
+++ TipCalcSwift/ViewController.swift
@@ -24,19 +24,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        billField.center = CGPointMake(160, 188)
-        tipControl.center = CGPointMake(160, 249.5)
-        dollarSignLabel.center = CGPointMake(287, 188)
+        billField.center = CGPointMake(view.center.x, 188) // billField.frame.size.height //188 // billField.frame.size.height/2 + billField.center.x + 74.5
+        tipControl.center = CGPointMake(view.center.x, 249.5)
+        dollarSignLabel.center = CGPointMake(dollarSignLabel.center.x, 188)
         
-        tipControl.alpha = 0
-        tipLabel.alpha = 0
-        totalLabel.alpha = 0
-        tipControl.alpha = 0
-        halfTotalLabel.alpha = 0
-        thirdTotalLabel.alpha = 0
-        forthTotalLabel.alpha = 0
-        backgroundColorView.alpha = 0
-        peopleLabelCollection.map { $0.alpha = 0 }
+        showInputOnly(true)
     }
 
     override func didReceiveMemoryWarning() {
@@ -52,78 +44,71 @@ class ViewController: UIViewController {
         
         
         if billField.text.isEmpty {
-            
-            
+
             UIView.animateWithDuration(duration, animations: { () -> Void in
-                self.tipControl.alpha = 0
-                self.tipLabel.alpha = 0
-                self.totalLabel.alpha = 0
-                self.tipControl.alpha = 0
-                self.halfTotalLabel.alpha = 0
-                self.thirdTotalLabel.alpha = 0
-                self.forthTotalLabel.alpha = 0
-                self.backgroundColorView.alpha = 0
-                self.peopleLabelCollection.map { $0.alpha = 0 }
+                self.showInputOnly(true)
                 self.dollarSignLabel.alpha = 1
             })
             
             UIView.animateWithDuration(duration, delay: delay, options: options, animations: {
-                self.billField.center = CGPointMake(160, 188)
-                self.tipControl.center = CGPointMake(160, 249.5)
-                self.dollarSignLabel.center = CGPointMake(287, 188)
                 
-                }, completion: { finished in
-                    // any code entered here will be applied
-                    // once the animation has completed
-            })
+                self.billField.center = CGPointMake(self.view.center.x, 188)
+                self.tipControl.center = CGPointMake(self.view.center.x, 249.5)
+                self.dollarSignLabel.center = CGPointMake(self.dollarSignLabel.center.x, 188)
+                
+                }, completion: nil )
             
         } else {
             self.dollarSignLabel.alpha = 0
             
             UIView.animateWithDuration(duration, animations: { () -> Void in
-                self.tipControl.alpha = 1
-                self.tipLabel.alpha = 1
-                self.totalLabel.alpha = 1
-                self.tipControl.alpha = 1
-                self.halfTotalLabel.alpha = 1
-                self.thirdTotalLabel.alpha = 1
-                self.forthTotalLabel.alpha = 1
-                self.backgroundColorView.alpha = 1
-                self.peopleLabelCollection.map { $0.alpha = 1 }
+                self.showInputOnly(false)
             })
             
             UIView.animateWithDuration(duration, delay: delay, options: options, animations: {
-                self.billField.center = CGPointMake(160, 117)
-                self.tipControl.center = CGPointMake(160, 171.5)
-                self.dollarSignLabel.center = CGPointMake(287, 117)
+                self.billField.center = CGPointMake(self.view.center.x, 117)
+                self.tipControl.center = CGPointMake(self.view.center.x, 171.5)
+                self.dollarSignLabel.center = CGPointMake(self.dollarSignLabel.center.x, 117)
                 
-                }, completion: { finished in
-                    // any code entered here will be applied
-                    // once the animation has completed
-            })
+                }, completion: nil )
         }
         
-        
-        let tipPercentages = [0.15, 0.18, 0.2]
-        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
-        
-        var billAmount = (billField.text as NSString).doubleValue
-
-        var tip = billAmount * tipPercentage
-        var total = billAmount + tip
-        var halfTotal = total / 2
-        var thirdTotal = total / 3
-        var forthTotal = total / 4
-
-        tipLabel.text = String(format: "$%.2f", tip)
-        totalLabel.text = String(format: "$%.2f", total)
-        halfTotalLabel.text = String(format: "$%.2f", halfTotal)
-        thirdTotalLabel.text = String(format: "$%.2f", thirdTotal)
-        forthTotalLabel.text = String(format: "$%.2f", forthTotal)
+        updateUI()
     }
     
     @IBAction func onTap(sender: AnyObject) {
         view.endEditing(true)
+    }
+    
+    func updateUI() {
+        let tipPercentages = [0.15, 0.18, 0.2]
+        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        var billAmount = (billField.text as NSString).doubleValue
+        
+        var tip = billAmount * tipPercentage
+        var total = billAmount + tip
+        let dollarFormat = "$%.2f"
+        
+        tipLabel.text        = String(format: dollarFormat, tip)
+        totalLabel.text      = String(format: dollarFormat, total)
+        halfTotalLabel.text  = String(format: dollarFormat, total / 2)
+        thirdTotalLabel.text = String(format: dollarFormat, total / 3)
+        forthTotalLabel.text = String(format: dollarFormat, total / 4)
+    }
+    
+    func showInputOnly(showInputOnly: Bool) {
+        let opactiy: CGFloat = showInputOnly ? 0 : 1
+        
+        tipControl.alpha = opactiy
+        tipLabel.alpha = opactiy
+        totalLabel.alpha = opactiy
+        tipControl.alpha = opactiy
+        halfTotalLabel.alpha = opactiy
+        thirdTotalLabel.alpha = opactiy
+        forthTotalLabel.alpha = opactiy
+        backgroundColorView.alpha = opactiy
+        peopleLabelCollection.map { $0.alpha = opactiy }
     }
 }
 
